57c48f027644b92304d8a0182157a2241f5123ad
Shaked Cohen <27896110+shaked113021@users.noreply.github.com>
2021-05-30 16:26:35 +0300

    implemented RandomStrategy

--- src/cpp/tictactoe/botstrategies/RandomStrategy.cpp
+++ src/cpp/tictactoe/botstrategies/RandomStrategy.cpp
@@ -1,9 +1,18 @@
 #include "tictactoe/botstrategies/RandomStrategy.h"
+#include <cmath>
 
 using namespace tictactoe::botstrategies;
 
 RandomStrategy::RandomStrategy(Board *board) : board_(board) {}
 
 void RandomStrategy::DoMove() {
-  // TODO: Implement Random strategy
+  int x;
+  int y;
+  
+  // generate random x and y until hitting a clear spot
+  do{
+    x = rand() % (kRowAndCollSize - 1);
+    y = rand() % (kRowAndCollSize - 1);
+  } while(this->board_->GetCell(x,y) != kUnused);
+  this->board_->SetCell(x, y, kBot);
 }
